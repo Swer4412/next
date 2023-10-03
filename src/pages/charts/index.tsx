3ca5eca15141434c9f23c405852cbda38738deda
Swer4412 <Swer4412@gmail.com>
2023-10-03 16:31:38 +0200

    roba

---
+++ src/pages/charts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            Pagina index charts
+        </div>
+    );
+};
+
+export default index;
---
+++ src/pages/charts/piechart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const piechart = () => {
+    return (
+        <div>
+            Pie chart
+        </div>
+    );
+};
+
+export default piechart;
